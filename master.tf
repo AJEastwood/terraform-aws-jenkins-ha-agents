bed1765a3b048aa87495a336ca70cd52bc70bdc3
Vinit Khandagle <79217316+vkbytes@users.noreply.github.com>
2023-10-27 09:22:17 +0100

    Fix tabs and space
    Fixing the tabs in the S3 role policy

--- master.tf
+++ master.tf
@@ -53,8 +53,8 @@ resource "aws_iam_role_policy" "master_inline_policy" {
       "Action": [
         "ec2:DescribeInstances",
         "s3:GetObject",
-			  "s3:GetObjectAcl",
-			  "s3:PutObject",
+	"s3:GetObjectAcl",
+	"s3:PutObject",
         "autoscaling:DescribeAutoScalingGroups"
       ],
       "Effect": "Allow",
